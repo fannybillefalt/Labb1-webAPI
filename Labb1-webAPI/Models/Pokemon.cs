f21496999f20bdee5f9e2db94954aa314e81c198
fannybillefalt <fannybillefalt@gmail.com>
2026-08-24 10:13:37 +0200

    Add JSON attributes and defaults to Pokemon class
    Added System.Text.Json.Serialization import and [JsonPropertyName] attributes for explicit JSON mapping. Initialized Name, Abilities, and Forms properties with default values.

--- Labb1-webAPI/Models/Pokemon.cs
+++ Labb1-webAPI/Models/Pokemon.cs
@@ -1,14 +1,27 @@
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace Labb1_webAPI.Models
 {
+    //Anser inte att annotations egentligen behövs men lägger till de för tydlighetens skull.
     public class Pokemon
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+
+        [JsonPropertyName("name")]
         public string Name { get; set; } = string.Empty;
+
+        [JsonPropertyName("height")]
         public int Height { get; set; }
+
+        [JsonPropertyName("weight")]
         public int Weight { get; set; }
+
+        [JsonPropertyName("abilities")]
         public List<string> Abilities { get; set; } = new List<string>();
+
+        [JsonPropertyName("forms")]
         public List<string> Forms { get; set; } = new List<string>();
     }
 }
